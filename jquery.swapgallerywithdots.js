d8f56908dc15450a61164f84156a68201abaa520
Takeshi Takatsudo <takazudo@gmail.com>
2013-06-13 20:02:41 +0900

    bumped version number to v0.1.0

--- jquery.swapgallerywithdots.js
+++ jquery.swapgallerywithdots.js
@@ -1,6 +1,6 @@
 /*! jQuery.swapgallerywithdots (https://github.com/Takazudo/jQuery.swapgallerywithdots)
  * lastupdate: 2013-06-13
- * version: 0.0.0
+ * version: 0.1.0
  * author: 'Takazudo' Takeshi Takatsudo <takazudo@gmail.com>
  * License: MIT */
 (function() {
