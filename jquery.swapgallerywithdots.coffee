caadae0458eb271fbec3d7d71789e520370d37ef
Takeshi Takatsudo <takazudo@gmail.com>
2013-06-13 20:01:38 +0900

    almost finished base implementation

--- jquery.swapgallerywithdots.coffee
+++ jquery.swapgallerywithdots.coffee
@@ -1,2 +1,104 @@
-$.fn.swapgallerywithdots = ->
-  console.log 'foo!'
+# encapsulate plugin
+do ($=jQuery) ->
+
+  ns = {}
+
+  # ============================================================
+  # Main
+
+  class ns.Main
+
+    @defaults =
+      selector_gallery_container: null
+      selector_gallery_inner: null
+      selector_gallery_item: null
+      selector_dot_container: null
+      selector_dot_item: null
+      class_dot_activeItem: null
+      class_dot_inactiveItem: null
+      src_dotItem: null
+    
+    constructor: (@$el, options) ->
+      
+      @options = $.extend {}, ns.Main.defaults, options
+      @_prepareTouchdragh()
+      @_putDotsHtml()
+      @_prepareDots()
+      @_eventify()
+    
+    countItems: ->
+      return (@$gallery.find @options.selector_gallery_item).length
+
+    _prepareTouchdragh: ->
+
+      o = @options
+      @$gallery = @$el.find o.selector_gallery_container
+
+      galleryOptions =
+        inner: o.selector_gallery_inner
+        item: o.selector_gallery_item
+
+      @$gallery.touchdraghfitty galleryOptions
+      @touchdragh = @$gallery.data 'touchdraghfitty'
+
+      return this
+
+    _putDotsHtml: ->
+
+      @$dots = @$el.find @options.selector_dot_container
+      
+      src = @options.src_dotItem
+      l = @countItems()
+      srcs = ''
+
+      for [0...l]
+        srcs += src
+
+      @$dots.append srcs
+
+      return this
+      
+    _prepareDots: ->
+
+      o = @options
+
+      dotOptions =
+        selector_item: o.selector_dot_item
+        class_activeItem: o.class_dot_activeItem
+
+      if o.class_dot_inactiveItem
+        dotOptions.class_inactiveItem = o.class_dot_inactiveItem
+
+      @$dots.currentDots dotOptions
+      @dot = @$dots.data 'currentDots'
+
+      # @dotInstance.to(100)
+      
+      return this
+
+    _eventify: ->
+      
+      @dot.on 'itemclick', (data) =>
+        @touchdragh.to data.index, true
+
+      @touchdragh.on 'indexchange', (data) =>
+        @dot.to data.index
+      
+      return this
+
+  # ============================================================
+  # bridge to plugin
+
+  $.fn.swapgallerywithdots = (options) ->
+    return @each (i, el) ->
+      $el = $(el)
+      instance = new ns.Main $el, options
+      $el.data 'swapgallerywithdots', instance
+      return
+
+  # ============================================================
+  # globalify
+
+  $.SwapgallerywithdotsNs = ns
+  $.Swapgallerywithdots = ns.Main
+
--- jquery.swapgallerywithdots.js
+++ jquery.swapgallerywithdots.js
@@ -5,8 +5,100 @@
  * License: MIT */
 (function() {
 
-  $.fn.swapgallerywithdots = function() {
-    return console.log('foo!');
-  };
+  (function($) {
+    var ns;
+    ns = {};
+    ns.Main = (function() {
+
+      Main.defaults = {
+        selector_gallery_container: null,
+        selector_gallery_inner: null,
+        selector_gallery_item: null,
+        selector_dot_container: null,
+        selector_dot_item: null,
+        class_dot_activeItem: null,
+        class_dot_inactiveItem: null,
+        src_dotItem: null
+      };
+
+      function Main($el, options) {
+        this.$el = $el;
+        this.options = $.extend({}, ns.Main.defaults, options);
+        this._prepareTouchdragh();
+        this._putDotsHtml();
+        this._prepareDots();
+        this._eventify();
+      }
+
+      Main.prototype.countItems = function() {
+        return (this.$gallery.find(this.options.selector_gallery_item)).length;
+      };
+
+      Main.prototype._prepareTouchdragh = function() {
+        var galleryOptions, o;
+        o = this.options;
+        this.$gallery = this.$el.find(o.selector_gallery_container);
+        galleryOptions = {
+          inner: o.selector_gallery_inner,
+          item: o.selector_gallery_item
+        };
+        this.$gallery.touchdraghfitty(galleryOptions);
+        this.touchdragh = this.$gallery.data('touchdraghfitty');
+        return this;
+      };
+
+      Main.prototype._putDotsHtml = function() {
+        var l, src, srcs, _i;
+        this.$dots = this.$el.find(this.options.selector_dot_container);
+        src = this.options.src_dotItem;
+        l = this.countItems();
+        srcs = '';
+        for (_i = 0; 0 <= l ? _i < l : _i > l; 0 <= l ? _i++ : _i--) {
+          srcs += src;
+        }
+        this.$dots.append(srcs);
+        return this;
+      };
+
+      Main.prototype._prepareDots = function() {
+        var dotOptions, o;
+        o = this.options;
+        dotOptions = {
+          selector_item: o.selector_dot_item,
+          class_activeItem: o.class_dot_activeItem
+        };
+        if (o.class_dot_inactiveItem) {
+          dotOptions.class_inactiveItem = o.class_dot_inactiveItem;
+        }
+        this.$dots.currentDots(dotOptions);
+        this.dot = this.$dots.data('currentDots');
+        return this;
+      };
+
+      Main.prototype._eventify = function() {
+        var _this = this;
+        this.dot.on('itemclick', function(data) {
+          return _this.touchdragh.to(data.index, true);
+        });
+        this.touchdragh.on('indexchange', function(data) {
+          return _this.dot.to(data.index);
+        });
+        return this;
+      };
+
+      return Main;
+
+    })();
+    $.fn.swapgallerywithdots = function(options) {
+      return this.each(function(i, el) {
+        var $el, instance;
+        $el = $(el);
+        instance = new ns.Main($el, options);
+        $el.data('swapgallerywithdots', instance);
+      });
+    };
+    $.SwapgallerywithdotsNs = ns;
+    return $.Swapgallerywithdots = ns.Main;
+  })(jQuery);
 
 }).call(this);
